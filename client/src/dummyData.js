02d7815835c4f27a77d70f930650ba518d955510
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-16 00:17:44 +0700

    Add dummy data file

---
+++ client/src/dummyData.js
@@ -0,0 +1,27 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "http://placekitten.com/32/32",
+    username: "Kucing Satu",
+  },
+  {
+    id: 2,
+    profilePicture: "http://placekitten.com/33/33",
+    username: "Kucing Dua",
+  },
+  {
+    id: 3,
+    profilePicture: "http://placekitten.com/34/34",
+    username: "Kucing Tiga",
+  },
+  {
+    id: 4,
+    profilePicture: "http://placekitten.com/35/35",
+    username: "Kucing Empat",
+  },
+  {
+    id: 5,
+    profilePicture: "http://placekitten.com/36/36",
+    username: "Kucing Lima",
+  },
+];
