e5e42c858d36e7dedc3ee12cc7b8b596e1d6e7d6
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-15 18:33:25 +0700

    Create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <h1>Hello Deric</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
