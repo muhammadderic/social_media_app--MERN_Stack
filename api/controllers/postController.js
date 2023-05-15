9c5c4546835fb2a25005be0e3cb35d8f4bd62bee
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-15 18:07:34 +0700

    Develop all post's functions

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,21 +1,47 @@
+const Post = require("../models/Post");
+
 // Create a Post
 const createPost = async (req, res) => {
-  res.send("Create a Post");
+  try {
+    const newPost = new Post(req.body);
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 // Get a Post
 const getPost = async (req, res) => {
-  res.send("Get a Post");
+  const id = req.params.id;
+  try {
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 // Update Post
 const updatePost = async (req, res) => {
-  res.send("Update Post");
+  const id = req.params.id;
+  try {
+    const post = await Post.findByIdAndUpdate(id, { $set: req.body });
+    res.status(200).json({ message: "Post has been updated" });
+  } catch (error) {
+    res.status(500).json({ message: error.messsage });
+  }
 }
 
 // Delete a Post
 const deletePost = async (req, res) => {
-  res.send("Delete a Post");
+  const id = req.params.id;
+  try {
+    await Post.findByIdAndDelete(id);
+    res.status(200).json({ message: "Post has been deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 module.exports = {
