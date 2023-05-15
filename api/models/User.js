4713ebdd44c3482b1e888432cc99430c636f3b12
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-15 14:23:34 +0700

    Create model for User and Post

---
+++ api/models/User.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    min: 3,
+    max: 20,
+  },
+  password: {
+    type: String,
+    require: true,
+    min: 6
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    max: 50,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  coverPicture: {
+    type: String,
+    default: "",
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  followings: {
+    type: Array,
+    default: [],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    max: 50,
+  },
+  city: {
+    type: String,
+    max: 20,
+  },
+  from: {
+    type: String,
+    max: 30,
+  },
+  relationship: {
+    type: Number,
+    enum: [1, 2, 3],
+  }
+},
+  { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
