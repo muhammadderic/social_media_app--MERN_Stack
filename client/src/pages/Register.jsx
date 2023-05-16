07722ec8135e140dbd0576a6e801a060e5073fd5
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-16 20:27:49 +0700

    Build register page and styling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,58 @@
+import { useRef } from "react";
 import "../styles/register.css";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+
   return (
-    <h1>Register</h1>
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Dericsocial</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Dericsocial.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox">
+            <input
+              placeholder="Username"
+              required
+              ref={username}
+              className="loginInput"
+            />
+            <input
+              placeholder="Email"
+              required
+              ref={email}
+              className="loginInput"
+              type="email"
+            />
+            <input
+              placeholder="Password"
+              required
+              ref={password}
+              className="loginInput"
+              type="password"
+              minLength="6"
+            />
+            <input
+              placeholder="Password Again"
+              required
+              ref={passwordAgain}
+              className="loginInput"
+              type="password"
+            />
+            <button className="loginButton" type="submit">
+              Sign Up
+            </button>
+            <button className="loginRegisterButton">Log into Account</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
