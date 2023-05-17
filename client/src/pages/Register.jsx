a6fba80622b4c90abff71bd0c1c8e07dacdaa4f6
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-17 23:31:04 +0700

    Completing register functionality and add error style at input password

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,41 @@
-import { useRef } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/register.css";
 
 export default function Register() {
-  const username = useRef();
-  const email = useRef();
-  const password = useRef();
-  const passwordAgain = useRef();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const [errorPassword, setErrorPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorPassword(false);
+    if (passwordAgain !== password) {
+      setErrorPassword(true);
+    } else {
+      const user = {
+        username,
+        email,
+        password,
+      }
+      try {
+        await fetch("http://localhost:5000/api/v1/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user)
+        })
+          .then(navigate("/login"));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   return (
     <div className="login">
@@ -17,34 +47,39 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <form className="loginBox">
+          <form className="loginBox" onSubmit={handleSubmit}>
             <input
               placeholder="Username"
               required
-              ref={username}
+              value={username}
               className="loginInput"
+              onChange={(e) => setUsername(e.target.value)}
             />
             <input
               placeholder="Email"
               required
-              ref={email}
+              value={email}
               className="loginInput"
               type="email"
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               placeholder="Password"
               required
-              ref={password}
+              value={password}
               className="loginInput"
               type="password"
               minLength="6"
+              onChange={(e) => setPassword(e.target.value)}
             />
+            <p className={errorPassword ? "errorPassword" : "showErrorPassword"}>Password is not same</p>
             <input
               placeholder="Password Again"
               required
-              ref={passwordAgain}
+              value={passwordAgain}
               className="loginInput"
               type="password"
+              onChange={(e) => setPasswordAgain(e.target.value)}
             />
             <button className="loginButton" type="submit">
               Sign Up
